c96ef7128b1e205616caaf80ff04009543e3037e
Fabio Marsiaj <fabiobackend@gmail.com>
2025-09-19 21:25:38 -0300

    Using fuzz for unit testing

--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		{"!12345", "54321!"},
-	}
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+
 	for _, tc := range testcases {
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev, tc.want)
-		}
+		f.Add(tc) // basically add each tc inputs for fuzz tests
 	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			t.Skip()
+		}
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			t.Skip()
+		}
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
