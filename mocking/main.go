13d11f97d3dd7ddb633bdd91799026ae641cb8f2
Fabio Marsiaj <fabiobackend@gmail.com>
2025-09-22 00:11:20 -0300

    mocking both Sleep and Write on SpyCountdownOperations

--- mocking/main.go
+++ mocking/main.go
@@ -9,23 +9,30 @@ import (
 
 const finalWord = "Go!"
 const countdownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
+type DefaultSleeper struct{}
+
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
-type DefaultSleeper struct{}
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
