ae68709bcd2e80588c105ae0860c872db4edefc4
Fabio Marsiaj <fabiobackend@gmail.com>
2025-09-27 03:54:37 -0300

    Implementing RequestLineParse, and made it pass on first tests

--- httpfromtcp/internal/request/request.go
+++ httpfromtcp/internal/request/request.go
@@ -1,9 +1,11 @@
 package request
 
 import (
+	"errors"
 	"io"
 	"log"
 	"strings"
+	"unicode"
 )
 
 type Request struct {
@@ -22,13 +24,60 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		log.Fatalf("Error reading content from Reader: %s\n", err.Error())
 	}
 	fullContent := string(content)
-	parseRequestLine(fullContent)
 
+	requestLine, err := parseRequestLine(fullContent)
+	if err != nil {
+		return nil, errors.New("fail creating RequestLine struct")
+	}
+
+	log.Printf("RequestLine created: %+v\n", requestLine)
+
+	return &Request{
+		RequestLine: *requestLine,
+	}, nil
 }
 
 func parseRequestLine(fullContent string) (*RequestLine, error) {
 	contentArr := strings.Split(fullContent, "\r\n")
 	requestLine := contentArr[0]
-	reqjuestLineBrokenDown := strings.Split()
+	reqLinesArr := strings.Split(requestLine, " ")
+	//  GET / HTTP/1.1
+	//  GET
+	//   /
+	//  HTTP/1.1
+	err := requestLineValidations(reqLinesArr)
 
+	if err != nil {
+		return nil, errors.New("fail on RequestLine validations")
+	}
+
+	method := reqLinesArr[0]
+	requestTarget := reqLinesArr[1]
+	httpVersion := strings.Split(reqLinesArr[2], "/")
+
+	return &RequestLine{
+		HttpVersion:   httpVersion[1],
+		RequestTarget: requestTarget,
+		Method:        method,
+	}, nil
+}
+
+func requestLineValidations(reqLinesArr []string) error {
+	method := strings.TrimSpace(reqLinesArr[0])
+	httpVersion := reqLinesArr[2]
+
+	for _, r := range method {
+		if !unicode.IsUpper(r) {
+			return errors.New("method contains lowercase letters")
+		}
+	}
+	log.Printf("Method is valid: %s\n", method)
+
+	httpSplit := strings.Split(httpVersion, "/")
+	if httpSplit[1] != "1.1" {
+		return errors.New("HTPP Version is not 1.1")
+	}
+	log.Printf("HTTP Version is valid: %s\n", httpSplit[1])
+
+	return nil
 }
